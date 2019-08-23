d13228f3ab8a74f3831219b499c3cc7d9188cc74
lelean <49761307+lelean@users.noreply.github.com>
2019-08-23 17:50:16 +0530

    Update number.sh

--- number.sh
+++ number.sh
@@ -1,6 +1,6 @@
 echo "First number is $num1" 
 echo "Second number is $num2" 
-echo "Addition of both the numbers is : $((num1 + num2))"
+echo "Addition of both the numbers is : $((num1 + num2)), added by Anil"
 echo "This script will now be pulled from webhook" 
 echo " New changes added" 
 echo "absahch" 
